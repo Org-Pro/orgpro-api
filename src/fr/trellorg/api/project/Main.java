f01ad9652f3e7039d8fade9bbe2f3c8b3adda1de
Thibault Sartre <32135105+ThibaultSartre@users.noreply.github.com>
2018-01-30 19:12:14 +0100

    Update Main.java

--- src/fr/trellorg/api/project/Main.java
+++ src/fr/trellorg/api/project/Main.java
@@ -4,6 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Share Project");
-        OrgHead task = new OrgHead("Aller faire les courses");
     }
 }
