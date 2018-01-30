53f1655c130fd54ebf740fd53a5f4761199be210
THIBAULT\sartr <sartre.thibault@hotmail.fr>
2018-01-30 19:04:50 +0100

    Test orgzly

--- src/fr/trellorg/api/project/Main.java
+++ src/fr/trellorg/api/project/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Share Project");
+        OrgHead task = new OrgHead("Aller faire les courses");
     }
 }
