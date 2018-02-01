57df2b8d6a41557051eecca6c9969efb5c905fbf
THIBAULT\sartr <sartre.thibault@hotmail.fr>
2018-02-01 17:06:52 +0100

    Update: Ajout de la classe Tache qui permet de créer une tache et de l'ajouter dans un fichier .org

--- src/fr/ShareProject/api/project/Tache.java
+++ src/fr/ShareProject/api/project/Tache.java
@@ -1,6 +1,14 @@
 package fr.ShareProject.api.project;
 
 import fr.ShareProject.api.orgzly.OrgHead;
+import fr.ShareProject.api.orgzly.OrgProperties;
+import fr.ShareProject.api.orgzly.parser.OrgParserWriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by sartr on 01/02/2018.
@@ -8,7 +16,71 @@ import fr.ShareProject.api.orgzly.OrgHead;
 public class Tache {
     OrgHead tache;
 
-    public Tache(String title,) {
+    OrgParserWriter ecriture;
+    SimpleDateFormat dateFormat;
 
+    public Tache(String title,String deadline,int level) {
+        ecriture = new OrgParserWriter();
+        this.tache = new OrgHead(title);
+        this.tache.setLevel(level);
+        dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date deadlineDate = dateFormat.parse(deadline);
+            tache.setDeadline(deadlineDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void ajoutTag(String[] tag){
+        tache.setTags(tag);
+    }
+
+    public void ajoutScheduled(String scheduled){
+        try {
+            Date scheduledDate = dateFormat.parse(scheduled);
+            tache.setScheduled(scheduledDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void ajoutClosed(String closed){
+        try {
+            Date closedDate = dateFormat.parse(closed);
+            tache.setClosed(closedDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void ajoutProperties(OrgProperties properties){
+        tache.setProperties(properties);
+    }
+
+    /*public void ajoutLogBook(String log,String logDate){
+        try {
+            log += new SimpleDateFormat("<yyyy-MM-dd>").format(dateFormat.parse(logDate));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        tache.addLog(log);
+    }*/
+
+    public void ecritureFichier(String path, boolean append){
+        OrgParserWriter ecriture = new OrgParserWriter();
+        String ecrire = ecriture.whiteSpacedHead(tache,tache.getLevel(),true);
+        try {
+            FileWriter ffw=new FileWriter(path,append);
+            ffw.write(ecrire);  // écrire une ligne dans le fichier resultat.txt
+            ffw.close(); // fermer le fichier à la fin des traitements
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return ecriture.whiteSpacedHead(tache,tache.getLevel(),true);
     }
 }
