aafdac30d976cc75ffaf9d6fe93d91a2d6bc1947
THIBAULT\sartr <sartre.thibault@hotmail.fr>
2018-02-01 17:14:11 +0100

    Update: Changement de la classe main pour créer une tache à l'aide de la nouvelle classe Tache

--- src/fr/ShareProject/api/project/Main.java
+++ src/fr/ShareProject/api/project/Main.java
@@ -8,45 +8,30 @@ import fr.ShareProject.api.orgzly.parser.OrgParserWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         System.out.println("Share Project");
-        OrgHead task = new OrgHead("Aller faire les courses");
-        OrgDateTime time = new OrgDateTime(true);
-        time = time.doParse("<2017-02-02>");
-        OrgRange deadline = new OrgRange(time);
-        task.setDeadline(deadline);
-        time = time.doParse("<2017-01-31>");
-        OrgRange scheduled = new OrgRange(time);
-        task.setScheduled(scheduled);
-        time = time.doParse("<2017-02-01>");
-        OrgRange closed = new OrgRange(time);
-        task.setClosed(closed);
-        task.setState("DONE");
+        Tache tache = new Tache("Faire les courses", "2018-02-03",1);
         String[] tags = new String[2];
         tags[0] = "COURSE";
         tags[1] = "URGENT";
-        task.setTags(tags);
+        tache.ajoutTag(tags);
+        tache.ajoutScheduled("2018-01-31");
+        tache.ajoutClosed("2018-02-02");
         OrgProperties properties = new OrgProperties();
         properties.put("LISTE_PRINCIPAL","riz,dinde,huile");
         properties.put("LISTE_SECONDAIRE","coca,gateaux");
-        task.setProperties(properties);
-        String log = "- State 'DONE' FROM 'TODO' " + closed;
-        task.addLog(log);
-        OrgParserWriter ecriture = new OrgParserWriter();
-        String ecrire = ecriture.whiteSpacedHead(task,1,true);
-        System.out.println(ecrire);
-
-        try {
-            File ff=new File("C://Users/sartr/OneDrive/Documents/Université/ProjetM1/liste.org");
-            ff.createNewFile();
-            FileWriter ffw=new FileWriter(ff);
-            ffw.write(ecrire);  // écrire une ligne dans le fichier resultat.txt
-            ffw.close(); // fermer le fichier à la fin des traitements
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        tache.ajoutProperties(properties);
+        /*String log = "- State 'DOING' FROM 'TODO' ";
+        tache.ajoutLogBook(log,"2018-02-02");
+        log = "- State 'DONE' FROM 'DOING' ";
+        tache.ajoutLogBook(log,"2018-02-02");*/
+        System.out.println(tache);
+        tache.ecritureFichier("C://Users/sartr/OneDrive/Documents/Université/ProjetM1/liste.org",false);
     }
 }
