8b929941ef04d762c3e03bd04ff1e5f3ccd65f4d
Alex-PC\Alex <walloisa.info@gmail.com>
2018-02-02 19:06:01 +0100

    Test build travis

--- src/main/java/fr/trellorg/api/project/Main.java
+++ src/main/java/fr/trellorg/api/project/Main.java
@@ -3,6 +3,6 @@ package fr.trellorg.api.project;
 public class Main {
 
     public static void main(String[] args) {
-        Siystem.out.println("Share Project");
+        System.out.println("Share Project");
     }
 }
