062bd052320c1884f87fc1bda9b0c34e05c73557
Baptiste Rayer <batrayer77@gmail.com>
2018-12-08 15:17:58 +0100

    Update: ajout de l'etat d'une tache lors de l'update

--- src/main/java/fr/orgpro/api/remote/google/GoogleStateEnum.java
+++ src/main/java/fr/orgpro/api/remote/google/GoogleStateEnum.java
@@ -1,8 +1,18 @@
 package fr.orgpro.api.remote.google;
 
 public enum GoogleStateEnum {
-    NEEDSACTION,
-    COMPLETED;
+    NEEDSACTION("needsAction"),
+    COMPLETED("completed");
+    private String gse;
+
+    GoogleStateEnum(String gse) {
+        this.gse = gse;
+    }
+
+    @Override
+    public String toString() {
+        return this.gse;
+    }
 
     public static GoogleStateEnum stringIsGoogleStateEnum(String val){
         try {
